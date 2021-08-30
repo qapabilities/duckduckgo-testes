56618f4bff7175c2fb31432d91b3a66aa9dd11cd
katimaia <katimaia@gmail.com>
2021-08-30 08:29:07 -0300

    Primeiros testes noo duckduckgo

--- cypress/integration/duckduckgo.spec.js
+++ cypress/integration/duckduckgo.spec.js
@@ -1,46 +1,67 @@
-const { it } = require("mocha")
+/// <reference types="Cypress" />
 
-describe('Buscas no duckduckgo.com', () => {
+describe('Tipos de Buscas', () => {
     beforeEach(() => {
         cy.visit('https://duckduckgo.com/')
     })
 
-it('Buscar pela lupa', () => {
+it('Buscar clicando na lupa', () => {
     cy.get('[name="q"]').type('cachorro vira-lata')
     cy.get('#search_button_homepage').click()
+    
     cy.get('.results.js-results').should('contain', 'cachorro vira-lata')
 })
 
-it('Buscar com o enter no campo', () => {
-    cy.get('.js-search-input.search__input--adv').type(`${'covid-23'}{enter}`)
-    cy.get('.results.js-results').should('contain', 'covid-23')   
+it('Buscar pressionando enter no campo', () => {
+    cy.get('.js-search-input.search__input--adv').type('covid-23{enter}')
+    
+    cy.contains('.results.js-results', 'covid-23').should('be.visible')
 })
 
-it("Deve exibir Título do filme destacado", () => {
+it('Buscar um filme, deve exibir Título do filme destacado', () => {
     cy.get('[name="q"]').type('covid-23')
     cy.get('#search_button_homepage').click()
+    
     cy.get('.module__title__link').should('contain', 'Songbird')
+    .should('be.visible')
 })
 
-it("Deve conter o atributo href no filme destacado", () => {
+it('Buscar um filme, deve conter link para mais informações', () => {
     cy.get('[name="q"]').type('covid-23')
     cy.get('#search_button_homepage').click()
-    cy.get('.module__image').should('have.attr', 'href')
-    //cy.get().should('contain', 'https://en.wikipedia.org/wiki/Songbird_(film)') ????????
+    
+    cy.get('.module__image').should('have.attr', 'href', 'https://en.wikipedia.org/wiki/Songbird_(film)')
 })
 
-it.onlyp("Deve clicar no filme destacado", () => {
-    cy.get('[name="q"]').type('covid-23')
-    cy.get('#search_button_homepage').click()
-    cy.get('.module--about__img').should('be.visible')
-    cy.get('.module--about__img').click({force:true})
-    cy.url().should('be.equal', 'https://en.wikipedia.org/wiki/Songbird_(film)')
-})
-
-it("Deve clicar em Mais resultados", () => {
+it('Buscar e clicar em "Mais resultados"', () => {
     cy.get('#search_form_input_homepage').type(`${'ornitorrinco'}{enter}`)
     cy.get('.results .result').should('have.length', 11)
     cy.get('.result--more__btn').click()
-    cy.get('.results .result').should('have.length', 31)
+    
+    cy.get('.results .result').should('have.length.above', 29)
+})
+
+it ('Filtrar o resultado da busca por país "Argentina"', () => {
+    cy.get('[name="q"]').type('covid')
+    cy.get('#search_button_homepage').click()
+    cy.get('.dropdown__switch').click()
+    cy.get('[data-id="ar-es"]') //assert pela linguagem que alterou?
+})
+
+it ('Filtrar o resultado da busca por "Mês passado"', ()=> {
+    cy.get('[name="q"]').type('covid')
+    cy.get('#search_button_homepage').click()
+    //cy.get('.dropdown--date').select('Mês passado') //como usar o .select do cypress?
+    cy.get('.dropdown--date').click()
+    cy.get(':nth-child(5) > .modal__list__link').should('be.visible').click({force:true})
+    // como seria o assert? (visualmente não tem data)
+ })
+
+it ('Buscar e visualizar "Imagens"', ()=> {
+    cy.get('[name="q"]').type('covid')
+    cy.get('#search_button_homepage').click()
+    cy.get('a[data-zci-link="images"]').click()
+        
+    cy.get('.zci__main--tiles').contains('covid').should('be.visible')
 })
 })
